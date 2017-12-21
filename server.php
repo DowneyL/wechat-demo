500d5f9b1afe78e0898ee19b836d065a4f6dd3d4
aragakiyui <heng8050lee@foxmail.com>
2017-12-21 15:17:50 +0800

    配置好微信连接

---
+++ server.php
@@ -0,0 +1,20 @@
+<?php
+use EasyWeChat\Factory;
+$config = [
+    'app_id' => 'wx9e2ecbc0419d67be',
+    'secret' => '4e6bf9993cb1da3f0bd9ece1aec9da6b',
+
+    'response_type' => 'array',
+
+    'log' => [
+        'level' => 'debug',
+        'file' => __DIR__.'/wechat.log',
+    ],
+];
+
+$app = Factory::officialAccount($config);
+
+$response = $app->server->serve();
+
+// 将响应输出
+$response->send();
