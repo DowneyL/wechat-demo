136c5281bb85637bf7e4a3a20188c144a36da1db
aragakiyui <heng8050lee@foxmail.com>
2017-12-22 12:44:13 +0800

    完成配置

---
+++ config/official_account.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+//    'debug' => true,
+    'app_id' => 'wx4b939851c5044ee8',
+    'secret' => '5588db2e011ef577cb60a3f31f2eb6e0',
+    'token' => 'aragakicyann',
+    'response_type' => 'array',
+    'log' => [
+        'level' => 'debug',
+        'file' => __DIR__.'/../EasyWeChat.log',
+    ],
+];
--- server.php
+++ server.php
@@ -1,26 +1,18 @@
 <?php
+require __DIR__.'/vendor/autoload.php';
+
 use EasyWeChat\Factory;
 
-$options = [
-    'app_id' => 'wx9e2ecbc0419d67be',
-    'secret' => '4e6bf9993cb1da3f0bd9ece1aec9da6b',
-    'token' => 'aragakicyann',
-    'log' => [
-        'level' => 'debug',
-        'file' => '/wechat.log',
-    ],
-];
+$config = require __DIR__.'/config/official_account.php';
+$app = Factory::officialAccount($config);
 
-$app = Factory::officialAccount($options);
-
-$server = $app->server;
-/*
-$user = $app->user;
-$server->push(function ($message) use ($user) {
-    $fromUser = $user->get($message['FromUserName']);
-
-    return "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
+//var_dump($config);exit();
+$app->server->push(function($message) use ($app) {
+    $user = $app->user->get($message['FromUserName']);
+    return '你好！ '.$user['nickname'];
 });
-*/
 
-$server->serve()->send();
+$response = $app->server->serve();
+
+
+$response->send();
