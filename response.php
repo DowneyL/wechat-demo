5af6959cdb878d72b83c7c0f0e91afd643f30e4d
aragakiyui <heng8050lee@foxmail.com>
2017-12-28 16:33:42 +0800

    模板消息发送

--- response.php
+++ response.php
@@ -17,5 +17,25 @@ $response = $broadcast->sendText("这是一条测试数据", $user['data']['open
 var_dump($response);
 */
 
+/* 针对单个用户发送消息
 $response  = $app->customer_service->message('这是一条针对单个用户的测试数据')->to('o5b7Lw_onq9noT0AHX7AOR-TwUeI')->send();
 var_dump($response);
+*/
+
+/**
+ * 模板消息
+ */
+
+$result = $app->template_message->send([
+    'touser' => 'o5b7Lw_IdHDPTxVYPNaEvCqVgUnw',
+    'template_id' => 'XVTGR9XqOBiI7BrUhTDvikTrqVSD9Z7LVcwwUacaCAA',
+    'url' => 'https://www.baidu.com',
+    'data' => [
+        'first' => ["感谢您购买本教程！", '#FFA54F'],
+        'title' => "《微信小程序的开发》",
+        'price' => "￥ 188",
+        'remark' => "如果在使用过程中，有任何问题，欢迎联系我们！",
+    ],
+]);
+
+var_dump($result);
